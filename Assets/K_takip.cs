da1a3e2b4bf6435ab5588e7dc6cc511ae4c03f67
Çağıl Dişbudak <cdisbudak07@gmail.com>
2022-02-16 19:42:21 +0300

    Initial commit

---
+++ Assets/K_takip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class K_takip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject targetObject;
+    public Vector3 camera;
+    public Vector3 targeted;
+    private Vector3 velocity = Vector3.zero;
+
+
+    public float smoothTime = 0.3f;
+
+
+    void LateUpdate()
+    {
+        targeted = targetObject.transform.position + camera;
+        transform.position = Vector3.SmoothDamp(transform.position, targeted, ref velocity, smoothTime);
+
+    }
+}
---
+++ Assets/Nature_pixel_art_assets/Kodlar/T_Hareket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T_Hareket : MonoBehaviour
+{
+    public float donushizi = -90;
+    public float hiz = 3;
+
+
+    private Rigidbody2D rigid;
+    void Start()
+    {
+
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigid.angularVelocity = Input.GetAxis("Horizontal") * donushizi;
+
+        rigid.velocity = transform.up * Input.GetAxis("Vertical") * hiz;
+
+
+
+    }
+}
